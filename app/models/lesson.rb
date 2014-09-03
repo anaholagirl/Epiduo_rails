b2ced0ee38857145099a99a47fc016860b6d63a3
Linda Gibson <student@epicodus.com>
2014-09-03 11:36:58 -0700

    validate uniqueness of lesson number

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,6 @@ class Lesson < ActiveRecord::Base
 
   validates :name, :presence => true, :uniqueness => true
 
-  validates :lesson_number, :presence => true
+  validates :lesson_number, :presence => true, :uniqueness => true
 
 end
--- spec/lessons/lesson_spec.rb
+++ spec/lessons/lesson_spec.rb
@@ -5,5 +5,7 @@ describe Lesson do
 
   it { should validate_uniqueness_of :name }
 
-  it {should validate_presence_of :lesson_number }
+  it { should validate_presence_of :lesson_number }
+
+  it { should validate_uniqueness_of :lesson_number }
 end
