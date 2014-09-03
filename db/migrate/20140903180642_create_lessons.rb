c884c7109c4516711c68c1717c72bad1fa3cfa26
Linda Gibson <student@epicodus.com>
2014-09-03 11:31:44 -0700

    validate presence of name of lesson

--- db/migrate/20140903180642_create_lessons.rb
+++ db/migrate/20140903180642_create_lessons.rb
@@ -1,6 +1,10 @@
 class CreateLessons < ActiveRecord::Migration
   def change
     create_table :lessons do |t|
+      t.column :name, :string
+      t.column :lesson_number, :integer
+
+      t.timestamps
     end
   end
 end
