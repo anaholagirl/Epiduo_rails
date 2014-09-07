3e72a8a8f27dc7b41398b0de2bcd10ffdcaa100e
Linda Gibson <dakidz@live.com>
2014-09-06 22:53:50 -0700

    add route, controller, index page for table of contents

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,50 @@
-class LessonsController < ApplicationController
+# class LessonsController < ApplicationController
 
-  def index
-    @lessons = Lesson.all
-    render('lessons/index.html.erb')
-  end
-end
+#   def index
+#     @lessons = Lesson.all
+#     render('lessons/index.html.erb')
+#   end
+
+#   def new
+#     @lesson = Lesson.new(params[:lesson])
+#     render('lessons/new.html.erb')
+#   end
+
+#   def create
+#     @lesson = Lesson.create(params[:lesson])
+#     if @lesson.save
+#       flash[:notice] = "Lesson was successfully added!"
+#       redirect_to("lessons/#{@lesson.id}")
+#     else
+#       render('lessons/new.html.erb')
+#   end
+
+#   def show
+#     @lesson = Lesson.find(params[:id])
+#     render('lessons/show.html.erb')
+#   end
+
+#   def edit
+#     @lesson = Lesson.find(params[:id])
+#     render('lessons/edit.html.erb')
+#   end
+
+#   def update
+#     @lesson = Lesson.find(params[:id])
+#     if @lesson.update(params[:lesson])
+#       flash[:notice] = "Update was successful!"
+#       redirect_to("lessons/#{@lesson.id}")
+#     else
+#       render('lessons/new.html.erb')
+#   end
+
+#   def destroy
+#     @lesson = Lesson.find(params[:id])
+#     @lesson.destroy
+#     flash[:notice] = "Lesson has been successfully removed!"
+#     redirect_to("lessons/#{@lesson.id}")
+#   end
+# end
 
 
 
